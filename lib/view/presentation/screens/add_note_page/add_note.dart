51f7be3a37f78618c5065794f375b64f4278211b
Muhammad fasil <fazilfaztml@gmail.com>
2023-10-04 17:59:50 +0530

    text feelds added and note adding

--- lib/view/presentation/screens/add_note_page/add_note.dart
+++ lib/view/presentation/screens/add_note_page/add_note.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AddNoteScreen extends StatelessWidget {
   const AddNoteScreen({super.key});
@@ -6,10 +7,32 @@ class AddNoteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("fdf")
-        ],
+      body: SafeArea(
+        child: Card(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    label: Text(
+                      'Title',
+                      style:
+                          GoogleFonts.poppins(color: Colors.grey, fontSize: 20),
+                    ),
+                    // border: OutlineInputBorder()
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: TextFormField(
+                decoration: const InputDecoration(border: InputBorder.none),
+                maxLines: 20,
+                maxLength: 150,
+              ))
+            ],
+          ),
+        ),
       ),
     );
   }
