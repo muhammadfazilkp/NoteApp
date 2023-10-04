77c43a2df08ab03258983fc7cac6dd1da3b927ec
Muhammad fasil <fazilfaztml@gmail.com>
2023-10-04 15:45:16 +0530

    navigated the add note page

--- lib/view/presentation/screens/homescreen/homescreen.dart
+++ lib/view/presentation/screens/homescreen/homescreen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:note_app/view/presentation/screens/add_note_page/add_note.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key});
+  const HomeScreen({super.key, Key? y});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -13,28 +14,31 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text('Notes',style: GoogleFonts.poppins(fontWeight: FontWeight.w600,color: Colors.purple),),
+        leading: Image.asset('asset/Screenshot 2023-10-04 122939.png'),
+        title: Text(
+          'Notes',
+          style: GoogleFonts.poppins(
+              fontWeight: FontWeight.w600, color: Colors.purple),
+        ),
       ),
       body: GridView.builder(
-        gridDelegate: const  SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, 
-          mainAxisSpacing: 8.0,
-          crossAxisSpacing: 8.0, 
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 5.0,
+          crossAxisSpacing: 8.0,
         ),
-        itemCount: 10, 
+        itemCount: 10,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.all(3.0),
             child: Card(
               elevation: 4.0,
               child: InkWell(
-                onTap: () {
-                  
-                },
+                onTap: () {},
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('Note $index', style: const  TextStyle(fontSize: 18.0)),
+                    Text('Note $index', style: const TextStyle(fontSize: 18.0)),
                     const SizedBox(height: 8.0),
                     const Text('This is a sample note.'),
                   ],
@@ -47,7 +51,13 @@ class _HomeScreenState extends State<HomeScreen> {
       floatingActionButton: FloatingActionButton.extended(
         backgroundColor: const Color.fromARGB(255, 112, 36, 204),
         onPressed: () {
+           Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const AddNoteScreen()));
           // Handle button press to create a new note
+
         },
         label: Text(
           'Create Note',
